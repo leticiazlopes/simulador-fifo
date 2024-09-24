c2d7f47cfb151f255d9efdca8a15621ca0c6178a
Suetone Carneiro <suetoneneto12@gmail.com>
2024-09-24 20:16:26 -0300

    :bug: fix: consertando o tempo total de execução

--- simulacao.py
+++ simulacao.py
@@ -7,12 +7,12 @@ def obter_tempo_chegada(processo):
 def fifo_escalonamento(processos):
     processos.sort(key=obter_tempo_chegada)
 
-    ultimo_processo = max([p.tempo_chegada for p in processos])
-    servico_total = 0
+    ultimo_processo = max([p.tempo_chegada for p in processos]) # descobre-se o último processo
+    tempo_total = 0
     for processo in processos:
-        servico_total += processo.tempo_servico
+        if processo.tempo_chegada == ultimo_processo:
+            tempo_total = processo.tempo_chegada + processo.tempo_servico # calcula-se o tempo total de execução
 
-    tempo_total = ultimo_processo + servico_total
     momento_atual = 0
     fila = []
     processo_em_andamento = None
